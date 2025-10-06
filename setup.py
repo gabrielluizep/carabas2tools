56fd18914bcb053dad6874d5abc53e8594001418
gabrielluizep <gabrielluizep@hotmail.com>
2025-10-06 19:28:04 -0300

    fix: added missing updates

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="carabas2tools",
     version="1.0.0",
-    description="A collection of tools to work with CARABAS-II dataset",
-    url="https://github.com/gabrielluizep/carabas2tools",
     author="Gabriel Luiz Espindola Pedro",
     author_email="gabrielluizep.glep@gmail.com",
-    packages=["carabas2tools"],
-    package_data={},
+    description="Tools to work with CARABAS-II dataset (requires local data)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabrielluizep/carabas2tools",
+    license="MIT",
+    packages=find_packages(),
     install_requires=["numpy", "scipy", "pandas"],
+    python_requires=">=3.8",
 )
